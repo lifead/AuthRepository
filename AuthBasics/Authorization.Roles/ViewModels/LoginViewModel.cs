e2f27b191e773864a45aef9ad1ac68df93b54903
Евгений <lifead@yandex.ru>
2022-02-02 15:36:01 +0300

    добавлена инициализация

--- AuthBasics/Authorization.Roles/ViewModels/LoginViewModel.cs
+++ AuthBasics/Authorization.Roles/ViewModels/LoginViewModel.cs
@@ -5,10 +5,10 @@ namespace Authorization.Roles.ViewModels
     public class LoginViewModel
     {
         [Required]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
         [Required]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
         [Required]
-        public string ReturnUrl { get; set; }
+        public string ReturnUrl { get; set; } = string.Empty;
     }
 }
